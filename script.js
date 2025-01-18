74c3b2e94f16728e1ae509f4e55870f7a18a6063
diaa <diaa58685@gmail.com>
2025-01-18 20:06:12 +0200

    .

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ async function loadData() {
             // Add card for Sheikh
             $('#content1').append(`
                 <div class="card">
-                    <img src="img/${element.image}" alt="${element.name}"/>
+                    <img src="${element.image}" alt="${element.name}"/>
                     <button alt="${element.name}" class="tablinks" data-active="${element.data_active}" data-id="${element.id}">${element.name}</button>
                 </div>
             `);
